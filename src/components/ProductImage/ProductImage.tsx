3ca54ebc3279919670146d5a050108d4bed4f8ab
Wilder <wilder_eduardo85@hotmail.com>
2023-01-28 13:59:24 -0300

    feat: Add image interaction

--- src/components/ProductImage/ProductImage.tsx
+++ src/components/ProductImage/ProductImage.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useState } from 'react'
 import styles from './productImage.module.css'
 import {
   product1,
@@ -9,34 +10,72 @@ import {
   product4,
   product4Thumbnail,
 } from '../../assets'
+import { Product } from '../../types'
 
-const thumbnails = [
-  { name: 'product-1', image: product1Thumbnail },
-  { name: 'product-2', image: product2Thumbnail },
-  { name: 'product-3', image: product3Thumbnail },
-  { name: 'product-4', image: product4Thumbnail },
+const products: Product[] = [
+  {
+    name: 'product-1',
+    image: product1,
+    thumbnailImage: product1Thumbnail,
+  },
+  {
+    name: 'product-2',
+    image: product2,
+    thumbnailImage: product2Thumbnail,
+  },
+  {
+    name: 'product-3',
+    image: product3,
+    thumbnailImage: product3Thumbnail,
+  },
+  {
+    name: 'product-4',
+    image: product4,
+    thumbnailImage: product4Thumbnail,
+  },
 ]
 
 function ProductImage() {
+  const [selectedProduct, setselectedProduct] = useState<Product>(products[0])
+
+  function onThumbnailClick(e: MouseEvent<HTMLButtonElement>) {
+    const target = e.target as HTMLImageElement
+
+    setselectedProduct(
+      products.find((product) => product.name === target.id) as Product
+    )
+  }
+
   return (
     <div className={styles.container}>
       <img
         className={styles.sneakersImage}
-        src={product1}
+        src={selectedProduct.image}
         alt='sneakers'
         width={450}
         height={450}
       />
       <div className={styles.thumbnailContainer}>
-        {thumbnails.map((thumbnail) => (
-          <img
-            key={thumbnail.name}
-            className={styles.thumbnail}
-            width={90}
-            height={90}
-            src={thumbnail.image}
-            alt={thumbnail.name}
-          />
+        {products.map(({ name, thumbnailImage }) => (
+          <button
+            className={`${styles.thumbnailButton} ${
+              name === selectedProduct.name ? styles.active : ''
+            }`}
+            key={name}
+            type='button'
+            onClick={onThumbnailClick}
+          >
+            <img
+              id={name}
+              className={`${styles.thumbnail} ${
+                name === selectedProduct.name ? styles.active : ''
+              }`}
+              width={90}
+              height={90}
+              src={thumbnailImage}
+              alt={name}
+            />
+          </button>
         ))}
       </div>
     </div>
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type Product = {
+  name: string
+  image: string
+  thumbnailImage: string
+}
